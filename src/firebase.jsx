5386e35cb040bbe4881f4b7fdde17766dbf1831b
OukSorphanDararath <dararoth.ouk28@gmail.com>
2024-06-22 14:05:38 +0700

    .

---
+++ src/firebase.jsx
@@ -0,0 +1,21 @@
+// src/firebase.js
+import { initializeApp } from "firebase/app";
+import { getStorage } from "firebase/storage";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDQ7jterUyEMMZhmopUStuPKOVPjNVxXgQ",
+  authDomain: "virtual-frontdesk.firebaseapp.com",
+  projectId: "virtual-frontdesk",
+  storageBucket: "virtual-frontdesk.appspot.com",
+  messagingSenderId: "201151179527",
+  appId: "1:201151179527:web:f7a372cb10ba03c02d33a2",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Firebase Storage
+const storage = getStorage(app);
+
+export { storage };
--- src/pages/Schedule.jsx
+++ src/pages/Schedule.jsx
@@ -8,23 +8,25 @@ import { RiDeleteBin6Line } from "react-icons/ri";
 import DeleteDialog from "../components/DeleteDialog";
 import Notify from "../components/Notify";
 import Loading from "../components/Loading";
+import { storage } from "../firebase";
+import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
 
 const apiBaseUrl = import.meta.env.VITE_API_KEY;
 
 const Schedule = () => {
-  const [schedulesData, setSchedulesData] = useState();
+  const [schedulesData, setSchedulesData] = useState([]);
   const [shiftName, setShiftName] = useState("");
   const [newShiftName, setNewShiftName] = useState("");
   const [shiftNameError, setShiftNameError] = useState("");
   const [newShiftNameError, setNewShiftNameError] = useState("");
   const [file, setFile] = useState(null);
   const [newFile, setNewFile] = useState(null);
-  const [selectedData, setSelectedData] = useState();
+  const [selectedData, setSelectedData] = useState(null);
   const [openDialog, setOpenDialog] = useState(false);
   const [showDeleteDialog, setShowDeleteDialog] = useState(false);
   const [notification, setNotification] = useState({ message: "", type: "" });
   const [clearFile, setClearFile] = useState();
-  const [isSummitting, setIsSubmitting] = useState(false);
+  const [isSubmitting, setIsSubmitting] = useState(false);
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
@@ -40,33 +42,30 @@ const Schedule = () => {
     }
   }, [selectedData]);
 
-  const getScheduleData = (id, afterRequest) => {
+  const getScheduleData = async (id, afterRequest) => {
     setIsLoading(true);
-    axios
-      .get(`${apiBaseUrl}/schedules`)
-      .then((response) => {
-        setSchedulesData(response?.data?.data);
-        if (id && afterRequest) {
-          setSelectedData(response?.data?.data?.find((x) => x._id === id));
-        } else if (!id && afterRequest) {
-          setSelectedData(
-            response?.data?.data[response?.data?.data.length - 1]
-          );
-        } else {
-          setSelectedData(response?.data?.data[0]);
-        }
+    try {
+      const response = await axios.get(`${apiBaseUrl}/schedules`);
+      setSchedulesData(response?.data?.data);
 
-        setIsLoading(false);
-      })
-      .catch(() => {
-        setNotification({ message: "Error fetching schedules", type: "error" });
-        setIsLoading(false);
-      });
+      if (id && afterRequest) {
+        setSelectedData(response?.data?.data?.find((x) => x._id === id));
+      } else if (!id && afterRequest) {
+        setSelectedData(response?.data?.data[response?.data?.data.length - 1]);
+      } else {
+        setSelectedData(response?.data?.data[0]);
+      }
+    } catch (error) {
+      setNotification({ message: "Error fetching schedules", type: "error" });
+    } finally {
+      setIsLoading(false);
+    }
   };
 
   const handleFileChange = (uploadedFile) => {
-    if (uploadedFile === null)
-      setSelectedData((prev) => ({ ...prev, pdf: null }));
+    if (uploadedFile === null) {
+      setSelectedData((prev) => ({ ...prev, pdfPath: null, pdfName: null }));
+    }
     if (openDialog) {
       setNewFile(uploadedFile);
     } else {
@@ -75,21 +74,20 @@ const Schedule = () => {
   };
 
   const handleShiftNameChange = (event, isNew) => {
+    const newName = event.target.value;
     if (isNew) {
-      setNewShiftName(event.target.value);
+      setNewShiftName(newName);
     } else {
-      setShiftName(event.target.value);
-      setSelectedData((prev) => ({ ...prev, name: event.target.value }));
+      setShiftName(newName);
+      setSelectedData((prev) => ({ ...prev, name: newName }));
     }
-    if (shiftNameError) setShiftNameError(""); // Clear error when user starts typing
-    if (newShiftNameError) setNewShiftNameError(""); // Clear error when user starts typing
+    if (shiftNameError) setShiftNameError("");
+    if (newShiftNameError) setNewShiftNameError("");
   };
 
   const handleDelete = async (id) => {
     try {
       const result = await axios.delete(`${apiBaseUrl}/schedules/${id}`);
-
-      // console.log("Form delete successfully:", result.data);
       getScheduleData();
       setShowDeleteDialog(false);
       setSelectedData(schedulesData.length > 0 ? schedulesData[0] : null);
@@ -102,7 +100,24 @@ const Schedule = () => {
     }
   };
 
-  // Handle form submission
+  const uploadFileToFirebase = async (file, path) => {
+    if (!file) return null;
+
+    try {
+      const storageRef = ref(storage, `${path}/${file.name}`);
+      await uploadBytes(storageRef, file);
+      const downloadURL = await getDownloadURL(storageRef);
+      console.log("File uploaded to Firebase:", {
+        path: downloadURL,
+        name: file.name,
+      }); // Debugging log
+      return { path: downloadURL, name: file.name };
+    } catch (error) {
+      console.error("Error uploading file to Firebase:", error); // Debugging log
+      throw error;
+    }
+  };
+
   const handleSubmit = async (event, id) => {
     event.preventDefault();
     setIsSubmitting(true);
@@ -117,28 +132,31 @@ const Schedule = () => {
     }
 
     if (isValid) {
-      // Log form data; files are optional
-      // console.log("Form submitted successfully:", { name, file });
-      const formData = new FormData();
-      if (id) {
-        formData.append("name", shiftName);
-        formData.append("file", file ?? selectedData?.pdf);
-      } else {
-        formData.append("name", newShiftName);
-        formData.append("file", newFile);
-      }
-
-      let result = {};
-
       try {
+        let fileData = {
+          path: selectedData?.pdfPath,
+          name: selectedData?.pdfName,
+        };
+
+        if (openDialog && newFile) {
+          fileData = await uploadFileToFirebase(newFile, "schedules");
+        } else if (!openDialog && file) {
+          fileData = await uploadFileToFirebase(file, "schedules");
+        }
+
+        console.log("File data to be sent in payload:", fileData); // Debugging log
+
+        const data = {
+          name: openDialog ? newShiftName : shiftName,
+          pdfPath: fileData?.path,
+          pdfName: fileData?.name,
+        };
+
+        let result;
         if (id) {
-          result = await axios.put(`${apiBaseUrl}/schedules/${id}`, formData, {
-            headers: { "Content-Type": "multipart/form-data" },
-          });
+          result = await axios.put(`${apiBaseUrl}/schedules/${id}`, data);
         } else {
-          result = await axios.post(`${apiBaseUrl}/schedules`, formData, {
-            headers: { "Content-Type": "multipart/form-data" },
-          });
+          result = await axios.post(`${apiBaseUrl}/schedules`, data);
         }
 
         setOpenDialog(false);
@@ -148,8 +166,8 @@ const Schedule = () => {
           type: "success",
         });
         getScheduleData(id, true);
-        setIsSubmitting(false);
       } catch (error) {
+        console.error("Error saving schedule:", error); // Debugging log
         setNotification({ message: "Error saving schedule.", type: "error" });
       }
     }
@@ -160,8 +178,6 @@ const Schedule = () => {
     <div className="h-full flex flex-col">
       <h1 className="mb-6 mt-2 font-semibold text-2xl">Schedule</h1>
       <div className="flex h-full gap-6">
-        {/* LIST CONTENT */}
-
         <div className="bg-[#283142] rounded-3xl flex p-4 flex-col w-64">
           {isLoading ? (
             <Loading />
@@ -174,32 +190,30 @@ const Schedule = () => {
               />
               <h6 className="font-semibold mt-6 mb-3 text-lg">Shifts</h6>
               <ul className="flex flex-col">
-                {schedulesData?.map((item) => {
-                  return (
-                    <li
-                      className={`border relative rounded-2xl px-5 py-2 cursor-pointer ${
-                        item?._id == selectedData?._id
-                          ? "bg-blue-700/30  border-white/10 text-blue-300"
-                          : "border-transparent"
-                      }`}
-                      key={item?._id}
-                      onClick={() => setSelectedData(item)}
+                {schedulesData?.map((item) => (
+                  <li
+                    className={`border relative rounded-2xl px-5 py-2 cursor-pointer ${
+                      item?._id === selectedData?._id
+                        ? "bg-blue-700/30  border-white/10 text-blue-300"
+                        : "border-transparent"
+                    }`}
+                    key={item?._id}
+                    onClick={() => setSelectedData(item)}
+                  >
+                    {item?.name}
+                    <button
+                      onClick={() => setShowDeleteDialog(true)}
+                      className="absolute right-3 top-2 p-1 rounded-full hover:bg-white/10 hover:text-red-400"
                     >
-                      {item?.name}{" "}
-                      <button
-                        onClick={() => setShowDeleteDialog(true)}
-                        className="absolute right-3 top-2 p-1 rounded-full hover:bg-white/10 hover:text-red-400 "
-                      >
-                        <RiDeleteBin6Line />
-                      </button>
-                    </li>
-                  );
-                })}
+                      <RiDeleteBin6Line />
+                    </button>
+                  </li>
+                ))}
               </ul>
             </>
           )}
         </div>
-        {/* EDIT CONTENT */}
+
         <div className="bg-[#283142] flex-1 p-10 rounded-3xl">
           {isLoading ? (
             <Loading />
@@ -207,9 +221,7 @@ const Schedule = () => {
             <>
               {schedulesData?.length > 0 ? (
                 <form
-                  onSubmit={(e) => {
-                    handleSubmit(e, selectedData._id);
-                  }}
+                  onSubmit={(e) => handleSubmit(e, selectedData._id)}
                   className="h-full flex flex-col gap-2"
                 >
                   <Input
@@ -224,7 +236,7 @@ const Schedule = () => {
 
                   <div className="mb-4 h-full">
                     <FileUpload
-                      filename={selectedData?.pdf}
+                      filename={selectedData?.pdfName}
                       fileType="pdf"
                       onFileChange={handleFileChange}
                       allowMultiple={false}
@@ -237,7 +249,7 @@ const Schedule = () => {
                       text={"Update"}
                       type="submit"
                       className={"w-52 border-2 border-white/20"}
-                      isLoading={!openDialog && isSummitting}
+                      isLoading={!openDialog && isSubmitting}
                     />
                   </div>
                 </form>
@@ -281,10 +293,8 @@ const Schedule = () => {
             setNewFile(null);
             setOpenDialog(false);
           }}
-          onFormSubmit={(e) => {
-            handleSubmit(e);
-          }}
-          isSubmitting={isSummitting}
+          onFormSubmit={(e) => handleSubmit(e)}
+          isSubmitting={isSubmitting}
         />
       )}
 
